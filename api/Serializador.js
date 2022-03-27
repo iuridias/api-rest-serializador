7693b5085a08da7532bb9d439cfb6a6f664d0ec8
Iuri Dias <iuridias.mw@gmail.com>
2022-03-27 16:20:16 -0300

    Projeto quase finalizado

---
+++ api/Serializador.js
@@ -0,0 +1,58 @@
+const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
+
+class Serializador {
+    json (dados) {
+        return JSON.stringify(
+          this.filtrar(dados)
+        )
+    }
+
+  serializar (dados) {
+    if (this.contentType === 'application/json') {
+      return this.json(dados);
+    }
+
+    throw new ValorNaoSuportado(this.contentType);
+  }
+
+  filtrarObjeto(dados) {
+    const novoObjeto = {};
+
+    this.camposPublicos.forEach(campo => {
+      if (dados.hasOwnProperty(campo))
+      novoObjeto[campo] = dados[campo];
+    })
+
+    return novoObjeto;
+  }
+
+  filtrar(dados) {
+    if (Array.isArray(dados)) {
+      dados = dados.map(item => {
+        return this.filtrarObjeto(item)
+      });
+    } else {
+      dados = this.filtrarObjeto(dados);
+    }
+
+    return dados;
+  }
+}
+
+class SerializadorFornecedor extends Serializador {
+  constructor (contentType) {
+    super();
+    this.contentType = contentType;
+    this.camposPublicos = [
+      'id',
+      'empresa',
+      'categoria'
+    ]
+  }
+}
+
+module.exports = {
+  Serializador: Serializador,
+  SerializadorFornecedor: SerializadorFornecedor,
+  formatosAceitos: ['application/json']
+}
